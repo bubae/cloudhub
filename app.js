1cc7f8da2e728120b64b37a75f3bcba6a1698e22
bubae <widianpear@gmail.com>
2016-04-25 14:59:31 +0900

    cloudhub server init

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var http = require('http');
+var fs = require('fs');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -7,12 +9,14 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var settings = require('./setting'); //Simple Setting Infos
 
 var app = express();
 
 // view engine setup
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,39 +26,68 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// app.use('/', routes);
+// app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// development only
+// if ('development' == app.get('env')) {
+//   app.use(express.errorHandler());
+// }
 
-// error handlers
+app.get('/', routes.main);
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+var error_404page = function(req, res){
+  req.render_info={
+    repository_id: null,
+    directView: false,
+    reviews: false,
+    user_name: undefined,
+    project_name: undefined,
+    repository_name: undefined,
+    reposerver_origin: settings.data.reposerver_origin,
+    name: false,
+    email: false
+  };  
+  res.render('404page', req.render_info);
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.get('*', error_404page);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+// // error handlers
+
+// // development error handler
+// // will print stacktrace
+// if (app.get('env') === 'development') {
+//   app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// }
+
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render('error', {
+//     message: err.message,
+//     error: {}
+//   });
+// });
+
+// ------ Create Server ------
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
 
 
-module.exports = app;
+// module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var crypto = require('crypto');
 var router = express.Router();
+var settings = require('../setting');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
-module.exports = router;
+// module.exports = router;
+
+
+
+exports.main = function(req, res){
+
+  if(!req.session || !req.user){
+    res.render('index');
+  }else{
+    res.redirect('/document');
+  }
+}
